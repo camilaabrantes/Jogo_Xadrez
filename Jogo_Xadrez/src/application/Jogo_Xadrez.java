2cce4906ea773defdfa643b144c2681ebbc49615
Camila <camila.abrantes@ee.ufcg.edu.br>
2021-07-11 22:18:27 -0300

    Commit 16: Listando pecas capturadas

--- Jogo_Xadrez/src/application/Jogo_Xadrez.java
+++ Jogo_Xadrez/src/application/Jogo_Xadrez.java
@@ -1,9 +1,10 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-import boardgame.BoardException;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -16,11 +17,12 @@ public class Jogo_Xadrez {
 		Scanner sc = new Scanner(System.in);
 
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);	
+				UI.printMatch(chessMatch, captured);	
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -36,6 +38,11 @@ public class Jogo_Xadrez {
 				
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
