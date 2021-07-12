01b4b690b2d72faf672f873fbdf29520fbf1991f
Camila <camila.abrantes@ee.ufcg.edu.br>
2021-07-11 21:07:43 -0300

    Commit 13: Imprimindo possiveis jogadas

--- Jogo_Xadrez/src/application/Jogo_Xadrez.java
+++ Jogo_Xadrez/src/application/Jogo_Xadrez.java
@@ -24,9 +24,16 @@ public class Jogo_Xadrez {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				//imprime movimento possiveis
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
+				
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
